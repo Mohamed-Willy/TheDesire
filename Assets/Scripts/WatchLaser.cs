7be21a7deb7a2a0171b1d816c68aeb65d4405439
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-04 17:54:15 +0300

    Raycast working now

--- Assets/Scripts/WatchLaser.cs
+++ Assets/Scripts/WatchLaser.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 
 public class WatchLaser : MonoBehaviour
 {
     LineRenderer lineRenderer;
+    Vector3 laserStartPosition;
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        laserStartPosition = lineRenderer.GetPosition(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.SetPosition(0, transform.position);
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        lineRenderer.SetPosition(0, Vector3.zero);
+        lineRenderer.SetPosition(1, new Vector3(0,0,5) * 10f);
+        if (Physics.Raycast(transform.position, transform.forward * 10, out hit))
         {
-            if (hit.collider)
-            {
-                lineRenderer.SetPosition(1, hit.point);
-            }
+            // if (hit.collider)
+            // {
+            // }
+            Debug.Log("Hit something which is: " + hit.transform.name);
         }
-        else lineRenderer.SetPosition(1, transform.forward*1000);
+        // else lineRenderer.SetPosition(1, transform.forward*1000);
+    }
+
+    void OnDrawGizmos () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * 10f);
     }
 }

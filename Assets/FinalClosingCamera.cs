f9713b59d77e75ffcca979fbf85d16c2aaac31a7
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-05 10:08:13 +0300

    Final Mohamed Push

---
+++ Assets/FinalClosingCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalClosingCamera : MonoBehaviour
+{
+    [SerializeField] List<GameObject> toEnable;
+    [SerializeField] List<GameObject> toDisable;
+
+    private void Start () {
+        foreach (GameObject obj in toEnable) {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in toDisable) {
+            obj.SetActive(true);
+        }
+
+        StartCoroutine(DisableAndEnable());
+    }
+    IEnumerator DisableAndEnable () {
+        yield return new WaitForSeconds(11.0f);
+        foreach (GameObject obj in toEnable) {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in toDisable) {
+            obj.SetActive(false);
+        }
+    }
+}
